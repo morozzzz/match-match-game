022e8d8486fd0b0de7013209b4a499f642800985
morozzzz <36655453+morozzzz@users.noreply.github.com>
2018-04-25 11:18:29 +0300

    modules added

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+import  { GameElement, Timer, OptionItem, Card } from './js/classes.js';
+
 const shirts = {
 	'shirt1' : "url('../img/shirt1.jpg')",
 	'shirt2' : "url('../img/shirt2.jpg')",
@@ -104,8 +106,6 @@ const chooseDifficulty = () => {
 };
 
 const switchOffTheLight = () => { document.querySelector('.light-switching-off').classList.add('switch-off') };
-
-
 const startGame = () => {
 	getUserInformation();
 	closeMainMenu();
@@ -200,8 +200,7 @@ const startGame = () => {
 		scoreTableTimeHead.pushInto(scoreTableHead.body);
 		scoreTableHead.pushInto(scoreTable.body);
 		goToMainMenuButton.body.classList.add('menu-button-final-screen');
-		
-
+	
 		for(let i = 1; i <= numberOfUsers; i++) {
 			const userProfile = JSON.parse(localStorage[`user${i}`]);
 			if(userProfile['counter']) users.push(userProfile); // to choose only users who won the game
@@ -222,7 +221,6 @@ const startGame = () => {
 			const newUserProfile = JSON.stringify(user);
 			localStorage[`user${i+1}`] = newUserProfile;
 		});
-
 		
 		scoreTable.pushInto(scoreTableWrapper.body);
 		goToMainMenuButton.pushInto(scoreTableWrapper.body);
---
+++ js/classes.js
@@ -0,0 +1,103 @@
+export class GameElement {
+	constructor(element,elementClassName) {
+		this.body = document.createElement(`${element}`);
+		this.body.classList.add(`${elementClassName}`);
+	}
+
+	pushInto(parentSelector) {
+		if(typeof parentSelector === 'string') {
+			const targetParent = document.querySelector(`${parentSelector}`);
+			targetParent.appendChild(this.body);
+		} else {
+			parentSelector.appendChild(this.body);
+		}       
+	}
+
+	addContent(content) {
+		this.body.innerHTML = `${content}`;
+	}
+
+	addEvent(eventName, calledFunction) {
+		this.body.addEventListener(`${eventName}`, calledFunction);
+	}
+
+	removeFrom(parentSelector) {
+		if(typeof parentSelector === 'string') {
+			const targetParent = document.querySelector(`${parentSelector}`);
+			targetParent.removeChild(this.body);
+		} else {
+			parentSelector.removeChild(this.body);
+		}       
+	}
+};
+
+export class Timer extends GameElement {
+	constructor(element,elementClassName) {
+		super(element,elementClassName);
+		this.timerId = null;
+		this.timerScreenValue = null;
+		this.timerCounter = 0;
+	}
+
+	start() {    
+		const timerScreen = this.body;
+		timerScreen.innerHTML = '0:0';
+		let m = 0, s = 0;        
+		const refreshTimer = () => {
+			this.timerCounter++;                       
+			s++;
+			if(s > 59) {
+				m++;
+				s = 0;
+			}		        
+			this.timerValue = [m, s];
+			this.timerScreenValue = this.timerValue.join(':');
+			timerScreen.innerHTML = this.timerScreenValue;
+		};
+		this.timerId = setInterval(refreshTimer,1000);                      
+	}
+    
+	stop() {    
+		clearInterval(this.timerId);        
+	}  
+    
+	saveValue() {
+		const numberOfLastUser = localStorage.length;
+		const currentUserIdentifier = `user${numberOfLastUser}`;
+		let currentUserProfile = JSON.parse(localStorage[currentUserIdentifier]);
+		currentUserProfile['time'] = this.timerScreenValue;
+		currentUserProfile['counter'] = this.timerCounter;
+		localStorage.setItem(currentUserIdentifier, JSON.stringify(currentUserProfile));	
+	}
+};
+
+export class OptionItem extends GameElement {
+	constructor(element,elementClassName,itemId,itemUrl) {
+		super(element,elementClassName);
+		this.body.id = itemId;
+		this.body.style.backgroundImage = itemUrl;
+	}
+};
+
+export class Card extends GameElement {
+	constructor(element,elementClassName,cardId,pairIdentificator) {
+		super(element,elementClassName);
+		this.body.classList.add(pairIdentificator);
+		this.body.id = cardId;
+	}
+
+	setShirt(shirtUrl) {
+		const shirt = document.createElement('div');
+		shirt.className = 'card-shirt'
+		shirt.style.backgroundImage = shirtUrl;
+		this.body.appendChild(shirt);
+
+	}
+
+	setFace(faceUrl) {
+		const face = document.createElement('div');
+		face.className = 'card-face'
+		face.style.backgroundImage = faceUrl;
+		this.body.appendChild(face);
+	}
+};
